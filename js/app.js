efec77804a724ba0950f02cc9fbeb9444097873b
Alfred <alfred@bufferapp.com>
2018-08-25 16:09:05 +0800

    Add flipping action
    Add CSS and Javascript for a (semi) card flipping animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-// JS
+//
+// 
+// Open card
+//
+//
+
+function openCard() {
+	event.target.classList.toggle('open');
+	event.target.classList.toggle('show');
+	console.log('A card is opened.');
+}
+
+document.addEventListener('click', openCard);
