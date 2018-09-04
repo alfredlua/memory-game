7ca02a657bfa36da45f1d90b3e0e4d3758d9b4b1
Alfred <alfred@bufferapp.com>
2018-09-04 09:50:29 +0800

    fix: Matching logic
    The matching logic was not working when I switched the title attribute to type. Will try it again next time.

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ function matchCard() {
 	let len = openedCards.length;
 	if (len === 2) {
 		addMove();
-		if (openedCards[0].type === openedCards[1].type) {
+		if (openedCards[0].title === openedCards[1].title) {
 			matched();
 		} else {
 			openedCards[0].classList.toggle('wrong');
